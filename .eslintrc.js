13373b86b995cffe879302f3567ed872394fb270
Vladislav Nikitin <dushekrad@gmail.com>
2020-03-25 17:00:48 +0400

    feat: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+      "@typescript-eslint",
+      "react",
+      "import",
+      "prettier",
+      "filenames",
+      "react-hooks",
+      "no-secrets",
+      "unicorn"
+    ],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript",
+      "plugin:unicorn/recommended",
+      "prettier",
+      "prettier/@typescript-eslint"
+    ],
+    rules: {
+      "filenames/match-regex": ["error", "^[a-z0-9.-]+$", false],
+      "filenames/no-index": "error",
+      "no-secrets/no-secrets": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "all",
+          singleQuote: false,
+          semi: false,
+          arrowParens: "always"
+        }
+      ]
+    }
+  };
