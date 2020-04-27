e2d50825973633f53b5b6a86708c3ff4cccad200
Vladislav Nikitin <dushekrad@gmail.com>
2020-04-27 15:06:06 +0400

    feat: make eslint work

--- src/main/container.tsx
+++ src/main/container.tsx
@@ -1,15 +1,19 @@
-import styled from "styled-components";
-import React, { ReactElement } from "react";
-import { OperatorCard } from "./ui/operator-card";
+import styled from "styled-components"
+import React, { ReactElement } from "react"
+import { OperatorCard } from "./ui/operator-card"
 
 const Main = styled.main`
-    width: 100%;
-    height: 100vh;
-    display: flex;
+  width: 100%;
+  height: 100vh;
+  display: flex;
 `
 
 const MainComponent = styled(Main)`
-    background: red;
+  background: red;
 `
 
-export const Container = (): ReactElement => <MainComponent><OperatorCard></OperatorCard></MainComponent>
+export const Container = (): ReactElement => (
+  <MainComponent>
+    <OperatorCard></OperatorCard>
+  </MainComponent>
+)
