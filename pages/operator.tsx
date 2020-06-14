fc60b10897bb0f7b02fd1e53cff8e1950e117ae4
Vladislav Nikitin <dushekrad@gmail.com>
2020-06-14 20:34:13 +0400

    feat: add operator page

---
+++ pages/operator.tsx
@@ -0,0 +1,3 @@
+import { OperatorPage } from "../src/operator/operator.page"
+
+export default OperatorPage
--- src/operator/operator.page.tsx
+++ src/operator/operator.page.tsx
@@ -1,3 +1,27 @@
-// import { ReactElement } from "react";
+import React, { ReactElement, Fragment, useEffect, useState } from "react"
+import { useRouter } from "next/dist/client/router"
+import Head from "next/head"
 
-// export const OperatorPage = (): ReactElement => <
+import operators from "../../operators-data/operators.json"
+
+export const OperatorPage = (): ReactElement => {
+  const router = useRouter()
+  const [operatorName, setOperatorName] = useState<string>()
+
+  const id = Number(router.query.id)
+
+  useEffect(() => {
+    setOperatorName(
+      operators.find((operator): boolean => operator.id === id)?.name,
+    )
+  })
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Оператор связи {operatorName}</title>
+      </Head>
+      {`operatorId: ${id}`}
+    </Fragment>
+  )
+}
