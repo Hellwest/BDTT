fe6efae1d512daf19f9d42c1c3ddf88d96da07e3
Vladislav Nikitin <dushekrad@gmail.com>
2020-07-02 15:58:56 +0400

    feat: add toasts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable filenames/match-regex */
 import React, { ReactElement, Fragment } from "react"
 import { default as NextApp } from "next/app"
+import { ToastContainer, toast } from "react-toastify"
 
 import { GlobalStyle } from "../theme/theme"
+
+import "react-toastify/dist/ReactToastify.css"
 
 class App extends NextApp {
   render(): ReactElement {
@@ -10,6 +13,9 @@ class App extends NextApp {
 
     return (
       <Fragment>
+        <ToastContainer
+          position={toast.POSITION.BOTTOM_RIGHT as "bottom-right"}
+        />
         <Component {...pageProps} />
         <GlobalStyle />
       </Fragment>
